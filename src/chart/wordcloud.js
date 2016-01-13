9dfbf2f036a0ee96feed2fcdd482eaac14d52f90
Yannick Assogba <yassogba@gmail.com>
2016-01-13 14:16:20 -0500

    Implement basic rendering of word cloud.
    Tokens are rendered in sorted order, with their scores displayed as superscript text. All text has the same orientation as well.
    
    Supports rendering multiple documents in a small multiples arrangement.

---
+++ src/chart/wordcloud.js
@@ -0,0 +1,107 @@
+import d3 from 'd3';
+import _ from 'lodash';
+
+export default class WordCloud {
+  constructor(opts) {
+    this.opts = opts;
+    this._container = opts.container;;
+    this.container = d3.select(this._container);
+  }
+
+  updateData(data) {
+    this.documents = _.cloneDeep(data);
+    this.documents.forEach((document) => {
+      document.tokens = _.sortBy(document.tokens, (t) => -t[1]);
+    });
+
+    // Create an array of maps that go from token -> score for each
+    // document in the data. Also keep track of how many documets each
+    // term appears in.
+    var documentAppearances = {};
+    var documentScores = this.documents.map((document) => {
+      var scores = document.tokens.reduce((map, [token, score]) => {
+        var currAppearences = documentAppearances[token] || 0;
+        documentAppearances[token] = currAppearences + 1;
+
+        map[token] = score;
+        return map;
+      }, {});
+      return scores;
+    });
+
+    // Create a map of all tokens to the sum of the scores in all the documents
+    this.sumScores = _.mergeWith({}, ...documentScores, (a, b) => {
+      if(a !== undefined && b !== undefined){
+        return a + b;
+      }
+      return undefined;
+    });
+
+    // Store the set of unique terms
+    this.uniqueTokens = new Set();
+    for (let key of Object.keys(documentAppearances)) {
+      let count = documentAppearances[key];
+      if (count === 1) {
+        this.uniqueTokens = this.uniqueTokens.add(key);
+      }
+    }
+  }
+
+  updateScales() {
+    this.fontSize = d3.scale.linear()
+      .domain(d3.extent(_.values(this.sumScores)))
+      .range([12, 64]);
+  }
+
+  update(data) {
+    this.updateData(data);
+    this.updateScales();
+  }
+
+  initialRender() {
+    // No-op
+  }
+
+  render() {
+
+    var wordCloud = this.container.selectAll('div.word-cloud-plot')
+      .data(this.documents);
+
+    var plot = wordCloud.enter()
+      .append('div')
+      .attr('class', 'word-cloud-plot');
+
+    plot
+      .append('div')
+      .attr('class', 'title')
+      .text((d) => d.name);
+
+    plot
+      .append('div')
+      .attr('class', 'cloud');
+
+    var tokens = plot.select('.cloud').selectAll('.tokens')
+      .data((d) => d.tokens, (d) => d[0]);
+
+    var token = tokens.enter()
+      .append('span')
+      .attr('class', 'token');
+
+    token
+      .append('span')
+      .attr('class', 'token-term')
+      .classed('unique', (d) => this.uniqueTokens.has(d[0]))
+      .style('font-size', (d) => this.fontSize(d[1]) + 'px')
+      .text((d) => d[0]);
+
+    token
+      .append('sup')
+      .attr('class', 'token-score')
+      .text((d) => d[1]);
+
+
+    tokens.exit().remove();
+    wordCloud.exit().remove();
+
+  }
+}
---
+++ src/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'; //eslint-disable-line
+import ReactDOM from 'react-dom';
+import WordCloud from './components/wordcloud/wordcloud.jsx';
+
+import '../index.html';
+
+// This will render out an an example of wordcloud
+
+import data from '../data/data.json';
+import config from '../data/config.json';
+
+document.addEventListener("DOMContentLoaded", function() {
+  ReactDOM.render(
+    <WordCloud
+      config={config}
+      data={data}
+    />,
+    document.querySelector("#main"));
+});
